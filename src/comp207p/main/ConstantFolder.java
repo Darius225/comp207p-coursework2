dcab6a10c31af47fdbfcf9987ade8345994bb44f
Darius <darius.grigore.pop10@gmail.com>
2018-04-15 17:59:24 +0100

    Initial commit

---
+++ src/comp207p/main/ConstantFolder.java
@@ -0,0 +1,138 @@
+package comp207p.main;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Iterator;
+
+import org.apache.bcel.classfile.*;
+import org.apache.bcel.util.InstructionFinder;
+import org.apache.bcel.generic.*;
+
+public class ConstantFolder
+{
+	ClassParser parser = null;
+	ClassGen gen = null;
+
+	JavaClass original = null;
+	JavaClass optimized = null;
+
+	public ConstantFolder(String classFilePath)
+	{
+		try{
+			this.parser = new ClassParser(classFilePath);
+			this.original = this.parser.parse();
+			this.gen = new ClassGen(this.original);
+		} catch(IOException e){
+			e.printStackTrace();
+		}
+	}
+	
+	public void optimize()
+	{
+		ClassGen classGen = new ClassGen(original);
+		ConstantPoolGen constPoolGen = classGen.getConstantPool();
+		Method[] methods = classGen.getMethods();
+		System.out.println("Working on " + classGen.getClassName());
+		for (Method method : methods) {
+			MethodGen methodGen = new MethodGen(method, classGen.getClassName(), constPoolGen);
+			System.out.println("Now optimizing method: " + method );
+            applySimpleFoldingToAMethod( methodGen , constPoolGen );
+            Method newMethod = methodGen.getMethod();
+            classGen.replaceMethod(method,newMethod);
+		}
+		this.optimized = classGen.getJavaClass();
+	}
+	public void applySimpleFoldingToAMethod( MethodGen methodGen , ConstantPoolGen constPoolGen)
+	{
+		boolean finished = false ;
+		InstructionList il = methodGen.getInstructionList() ;
+		String pat = "LDC LDC ArithmeticInstruction" ;
+		while ( finished == false )
+		{
+		        InstructionFinder finder = new InstructionFinder(il);
+                finished = true ;
+		        for(Iterator it = finder.search(pat); it.hasNext();) { // Iterate through instructions to look for arithmetic optimisation
+                    finished = false ;
+					InstructionHandle[] match = (InstructionHandle[]) it.next();
+					InstructionHandle firstPart , secondPart , operation ;
+					Number leftValue, rightValue;
+					firstPart = match [ 0 ] ;
+					secondPart = match [ 1 ] ;
+					operation = match [ 2 ] ;
+					leftValue = (Number) ((LDC) firstPart.getInstruction()).getValue(constPoolGen) ;
+					rightValue = (Number) ((LDC) secondPart.getInstruction()).getValue(constPoolGen) ;
+					ArithmeticInstruction op = (ArithmeticInstruction) operation.getInstruction();
+					System.out.println("First number is: " + leftValue + " and second :  " + rightValue + " Operation: " + op.getName().toString() ) ;
+					computeArithmeticalExpression( leftValue , rightValue , op , constPoolGen , firstPart );
+
+					try {
+
+						il.delete(secondPart);
+
+						il.delete(operation);
+
+						System.out.println("Am obtinut : " + (Number) ((LDC) firstPart.getInstruction()).getValue(constPoolGen) ) ;
+					}
+					catch (TargetLostException e)
+					{
+						e.printStackTrace();
+					}
+
+		        }
+		}
+		Instruction[] instruction = il.getInstructions() ;
+		for ( Instruction instruction1 : instruction )
+		{
+			System.out.println(instruction1.toString(false));
+		}
+	}
+	public void computeArithmeticalExpression ( Number a , Number b , ArithmeticInstruction arithmeticalInstruction , ConstantPoolGen constantPoolGen , InstructionHandle instructionHandle ) {
+		String code = arithmeticalInstruction.getName().toString();
+		if ( code.contains("add") ) {
+             if ( code.charAt(0) == 'i' )
+			 {
+			 	 Integer first = a.intValue();
+			 	 Integer second = b.intValue();
+			 	 Integer result = first + second ;
+			 	 int pos = constantPoolGen.addInteger(result);
+				 LDC newInstruction = new LDC(pos);
+				 instructionHandle.setInstruction(newInstruction);
+			 	 System.out.println("Rezultatul adunarii : " + first + '+' + second + " inserat in constant pool la : " + pos + " este: " + result ) ;
+			 }
+			 if ( code.charAt(0) == 'd' )
+			 {
+			 	 Double first = a.doubleValue();
+			 	 Double second = b.doubleValue();
+
+			 }
+		}
+		else if ( code.contains("mul") )
+		{
+
+     	}
+     	else if ( code.contains("sub") )
+		{
+
+		}
+		else if ( code.contains("div") )
+		{
+
+		}
+	}
+	public void write(String optimisedFilePath)
+	{
+		this.optimize();
+
+		try {
+			FileOutputStream out = new FileOutputStream(new File(optimisedFilePath));
+			this.optimized.dump(out);
+		} catch (FileNotFoundException e) {
+			// Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
---
+++ src/comp207p/target/SimpleFolding.j
@@ -0,0 +1,24 @@
+; Jasmin Java assembler code that assembles the SimpleFolding example class
+
+.source Type1.j
+.class public comp207p/target/SimpleFolding
+.super java/lang/Object
+
+.method public <init>()V
+	aload_0
+	invokenonvirtual java/lang/Object/<init>()V
+	return
+.end method
+
+.method public simple()V
+	.limit stack 3
+
+	getstatic java/lang/System/out Ljava/io/PrintStream;
+	ldc 135
+	ldc 12345
+	ldc 190
+	iadd
+    iadd
+    invokevirtual java/io/PrintStream/println(I)V
+	return
+.end method
